5fa5c01e3f26c87e4512872510ecd1c55f4a5b75
dyinnz <ml_143@sina.com>
2016-10-31 21:40:37 +0800

    init commit

---
+++ input/dy-debug.c
@@ -0,0 +1,53 @@
+printf("hello \" world");
+
+/*
+int x;
+int x, y = 1, z=2;
+
+x = y + z;
+
+x = -10 + hehe ;
+printf("I have a pen. %d\n", pen);
+x = a;
+x = a++;
+x = -a++;
+x = -a++ * b;
+x = -a++ * b - c;
+x = 10 < -a++ * b - c;
+x = 1 == 10 < -a++ * b - c;
+x = hello = world = 1 == 10 < -a++ * b - c;
+x = hello = world = 1 == 10 < -a++ * b - c, 999;
+int x;
+{
+x = 1;
+{
+int y;
+}
+
+}
+
+// main() {
+  int x = 5;
+  if (x < 10)
+    x = 1;
+   else if (x > 5) {
+    x = 2;
+  } else {
+    x = 3;
+  }
+
+  for (int i = 0; i < 100; i++) {
+    x = x + i;
+  }
+
+for (;;) {
+x = x + 2;
+break;
+}
+
+do {
+break;
+while (x < 10) x--;
+} while (0);
+// }
+ */
